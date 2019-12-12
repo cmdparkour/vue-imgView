e218b891f33fa26a23e1e491cd5242ff91251b59
罗茜 <1031596654@qq.com>
2019-12-12 15:16:16 +0800

    包发布成功，待测试，包名： vue-lookimg

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <hello-world />
   </div>
 </template>
+
+<script>
+import helloWorld from './components/HelloWorld'
+export default {
+  components: {
+    helloWorld
+  },
+  data() {
+    return {
+      layer: {
+        show: false,
+        active: '',
+        data: []
+      }
+    }
+  },
+  methods: {
+    // 打开弹窗
+    openView() {
+      this.layer.show = true
+      this.layer.active = 'http://blog.51weblove.com/blog/wp-content/uploads/2018/12/2018120513264371.jpg'
+      this.layer.data = [
+        'http://blog.51weblove.com/blog/wp-content/uploads/2018/12/2018120513264371.jpg',
+        'http://blog.51weblove.com/blog/wp-content/themes/grace8.2.1/timthumb.php?src=http://blog.51weblove.com/blog/wp-content/uploads/2019/12/2019120611565250.jpg&h=284&w=710&zc=1',
+        'http://blog.51weblove.com/blog/wp-content/themes/grace8.2.1/timthumb.php?src=http://blog.51weblove.com/blog/wp-content/uploads/2019/12/2019120511570034-e1575518238834.jpeg&h=284&w=710&zc=1',
+        'http://blog.51weblove.com/blog/wp-content/uploads/2018/12/2018120513104820.jpg',
+        'http://blog.51weblove.com/blog/wp-content/uploads/2018/12/2018120513105920.jpg',
+        'http://blog.51weblove.com/blog/wp-content/uploads/2019/03/2019032323443850.jpg'
+      ]
+    }
+  }
+}
+</script>
 
 <style>
 #app {
